821c36643c5a4f62d0ea38abac8d609ed7f8d8a7
DevAndersen <martinba.private@gmail.com>
2022-09-01 23:35:33 +0200

    SIte, formatting and tweaks

--- site/DevAndersen.Pi.Site.Web/Models/ErrorViewModel.cs
+++ site/DevAndersen.Pi.Site.Web/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace DevAndersen.Pi.Site.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace DevAndersen.Pi.Site.Web.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
