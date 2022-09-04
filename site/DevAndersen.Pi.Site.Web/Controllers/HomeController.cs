27ae0fef0bcf51a16663af0f8b7164dd0f37ef9b
DevAndersen <martinba.private@gmail.com>
2022-09-04 16:21:47 +0200

    Home buttons are now dynamic

--- site/DevAndersen.Pi.Site.Web/Controllers/HomeController.cs
+++ site/DevAndersen.Pi.Site.Web/Controllers/HomeController.cs
@@ -8,12 +8,10 @@ public class HomeController : ViewController
 {
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
+        return View(new HomeViewModel(new HomeButtonModel[]
+        {
+            CreateHomeViewModel<MediaController>(nameof(MediaController.Index), "Media", "bi-play-btn-fill", "#2d3e8c")
+        }));
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -23,5 +21,15 @@ public class HomeController : ViewController
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
         });
+    }
+
+    private static HomeButtonModel CreateHomeViewModel<TController>(string action, string text, string iconClass, string backgroundColor) where TController : Controller
+    {
+        return new HomeButtonModel(NameWithoutControllerSuffix<TController>(), action, text, iconClass, backgroundColor);
+    }
+
+    private static HomeButtonModel CreateHomeViewModel<TController>(Func<IActionResult> action, string text, string iconClass, string backgroundColor) where TController : Controller
+    {
+        return CreateHomeViewModel<TController>(action.Method.Name, text, iconClass, backgroundColor);
     }
 }
---
+++ site/DevAndersen.Pi.Site.Web/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DevAndersen.Pi.Site.Web.Models;
+
+public class HomeViewModel
+{
+    public HomeButtonModel[] Buttons { get; set; }
+
+    public HomeViewModel(HomeButtonModel[] buttons)
+    {
+        Buttons = buttons;
+    }
+}
+
+public record HomeButtonModel(string Controller, string Action, string Text, string IconClass, string BackgroundColor);
