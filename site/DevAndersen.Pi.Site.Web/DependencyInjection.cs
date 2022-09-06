0b9a6bf2340182b7321b618f5c68ee4248ba0eae
DevAndersen <martinba.private@gmail.com>
2022-09-06 02:10:38 +0200

    Moved DI to separate file

---
+++ site/DevAndersen.Pi.Site.Web/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using DevAndersen.Pi.Site.Core.Services.Abstractions;
+using DevAndersen.Pi.Site.Core.Services;
+
+namespace DevAndersen.Pi.Site.Web;
+
+internal static class DependencyInjection
+{
+    public static void Inject(IServiceCollection services)
+    {
+        services.AddTransient<IMediaService, MediaService>();
+    }
+}
--- site/DevAndersen.Pi.Site.Web/Program.cs
+++ site/DevAndersen.Pi.Site.Web/Program.cs
@@ -1,12 +1,11 @@
 using DevAndersen.Pi.Site.Core;
-using DevAndersen.Pi.Site.Core.Services;
-using DevAndersen.Pi.Site.Core.Services.Abstractions;
+using DevAndersen.Pi.Site.Web;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IMediaService, MediaService>();
+DependencyInjection.Inject(builder.Services);
 
 WebApplication app = builder.Build();
 
@@ -18,12 +17,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
